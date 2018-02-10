94ccdb9888b6d8d109473be266da91154b2a2a76
Zushisan <74838@supinfo.com>
2018-02-10 03:29:10 +0000

    made a basic layout for the main page with all the maps

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,26 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });
 
-  $(".toronto").on('click', function (event) {
 
     $.ajax({
       method: "GET",
       url: "/maps"
     }).done((maps) => {
       for(map of maps) {
-        $("<button class=\"map-button\" data-attr-id="+map.id+">").text(map.title).appendTo($("body"));
+        $(`<button class="map-button" data-attr-id="${map.id}"></button>`).text(map.title).appendTo($(".food"));
       }
     });
-    event.preventDefault();
-  });
+
 
   $("body").on('click', '.map-button', function (event){
     let dataValue = ($(this).attr("data-attr-id"));
-
     $.ajax({
       method: "GET",
       url: `/maps/${dataValue}`
